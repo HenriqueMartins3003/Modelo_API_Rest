82e22ba75e65cc24ced5c8085e82b1161e0eacb0
Henrique Martins <hiikemartins@gmail.com>
2023-05-11 23:35:12 -0300

    Atualizacoes para o deploy

--- server.js
+++
@@ -1,8 +0,0 @@
-import app from './app';
-
-const port = 3001;
-app.listen(port, () => {
-  console.log();
-  console.log(`Servidor escutando na porta:${port}`);
-  console.log(`CRTL + clique em http://localhost:${port}`);
-});
