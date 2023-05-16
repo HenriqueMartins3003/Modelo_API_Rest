9c864e23d7d3e5896f0e012f16ec3af2386f3a05
Henrique Martins <hiikemartins@gmail.com>
2023-05-16 09:24:13 -0300

    Build da API com o cors

--- dist/app.js
+++ dist/app.js
@@ -6,6 +6,8 @@ _dotenv2.default.config();
 require('./database');
 var _express = require('express'); var _express2 = _interopRequireDefault(_express);
 var _path = require('path');
+var _cors = require('cors'); var _cors2 = _interopRequireDefault(_cors);
+var _helmet = require('helmet'); var _helmet2 = _interopRequireDefault(_helmet);
 
 var _homeRoutes = require('./routes/homeRoutes'); var _homeRoutes2 = _interopRequireDefault(_homeRoutes);
 var _userRoutes = require('./routes/userRoutes'); var _userRoutes2 = _interopRequireDefault(_userRoutes);
@@ -13,6 +15,20 @@ var _tokenRoutes = require('./routes/tokenRoutes'); var _tokenRoutes2 = _interop
 var _alunoRoutes = require('./routes/alunoRoutes'); var _alunoRoutes2 = _interopRequireDefault(_alunoRoutes);
 var _pictureRoutes = require('./routes/pictureRoutes'); var _pictureRoutes2 = _interopRequireDefault(_pictureRoutes);
 
+const whiteList = [
+  'http://localhost:3000',
+  'http://192.168.0.1',
+];
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whiteList.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not Allowed by CORS'));
+    }
+  },
+};
 class App {
   constructor() {
     this.app = _express2.default.call(void 0, );
@@ -21,6 +37,8 @@ class App {
   }
 
   middlewares() {
+    this.app.use(_cors2.default.call(void 0, corsOptions));
+    this.app.use(_helmet2.default.call(void 0, ));
     this.app.use(_express2.default.urlencoded({ extended: true }));
     this.app.use(_express2.default.json());
     this.app.use(_express2.default.static('/images/', _path.resolve.call(void 0, __dirname, '..', 'uploads', 'images')));
